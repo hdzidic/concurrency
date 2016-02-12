acf54389330854b4f85f8d79d9c3838db1a9b4d2
Haris Dzidic <haris.dzidic@gmail.com>
2016-02-12 15:19:41 +0100

    Added logic

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 const database = require('./database');
 console.log('hello world');
 
-
 database.connect();
+
+const knex = database.knex;
+
+for (var i=0;i<50;i++) {
+  insert(Math.floor(Math.random()*50+1));
+}
+
+function insert (num) {
+  knex.transaction(function(trx) {
+   return trx
+   .raw('set transaction isolation level serializable;')
+   .then(function() {
+      return knex('test')
+      .where({
+        testnumber: num
+      })
+      .select('testnumber')
+      .transacting(trx);
+    })
+    .then(function(res) {
+      if (res && res.length > 0) {
+        return res;
+      } else {
+        return knex('test').returning('testnumber').insert({testnumber: num}).transacting(trx);
+      }
+    })
+    .then(trx.commit)
+    .catch(trx.rollback)
+   })
+   .then(function(inserts) {
+     console.log(num + ' saved or exists');
+   })
+   .catch(function(error) {
+     console.log(num + ' failed');
+   })
+   .done();
+}
