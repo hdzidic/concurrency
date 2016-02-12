5243c7f317430357b87e3b267b3e3c9a43e3ff7e
Haris Dzidic <haris.dzidic@gmail.com>
2016-02-12 15:54:43 +0100

    Added promises

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const database = require('./database');
 console.log('hello world');
+var Q = require('q');
 
 database.connect();
 
 const knex = database.knex;
+var promises = [];
+var failed=0;
+var success=0;
 
 for (var i=0;i<50;i++) {
-  insert(Math.floor(Math.random()*50+1));
+  promises.push(insert(Math.floor(Math.random()*50+1)));
 }
+Q.allSettled(promises)
+.then(function (results) {
+    results.forEach(function (result) {
+        if (result.state === "fulfilled") {
+          success++;
+        } else {
+          failed++;
+        }
+    });
+    console.log(`${success} jobs passed and ${failed} failed`);
+    process.exit();    
+})
 
 function insert (num) {
-  knex.transaction(function(trx) {
+  return knex.transaction(function(trx) {
    return trx
    .raw('set transaction isolation level serializable;')
    .then(function() {
@@ -30,7 +46,9 @@ function insert (num) {
     })
     .then(trx.commit)
     .catch(trx.rollback)
-   })
+  });
+}
+/*
    .then(function(inserts) {
      console.log(num + ' saved or exists');
    })
@@ -39,3 +57,4 @@ function insert (num) {
    })
    .done();
 }
+*/
